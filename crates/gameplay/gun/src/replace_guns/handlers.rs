59b57a850fa3d803538615e20de8e20ad4b41b01
dossancto <dossantossilvalucas963@gmail.com>
2026-04-07 23:52:52 -0300

    ajust code to gun replaced

--- crates/gameplay/gun/src/replace_guns/handlers.rs
+++ crates/gameplay/gun/src/replace_guns/handlers.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 
-use crate::{configuration::gun_components::ActiveGun, replace_guns::events::TakeGunEvent};
+use crate::{
+    configuration::gun_components::{ActiveGun, Gun},
+    replace_guns::events::TakeGunEvent,
+};
 
 pub(super) fn plugin(app: &mut App) {
     app.add_observer(handle_gun_replacement);
@@ -10,7 +13,15 @@ pub fn handle_gun_replacement(
     on: On<TakeGunEvent>,
     mut commands: Commands,
     active_guns_query: Query<Entity, With<ActiveGun>>,
+    guns_q: Query<Entity, With<Gun>>,
 ) {
+    let len = guns_q.iter().len();
+
+    if len >= 2 {
+        let current_gun_entity = active_guns_query.iter().next().unwrap();
+        commands.entity(current_gun_entity).despawn();
+    }
+
     for active_gun in active_guns_query.iter() {
         commands.entity(active_gun).remove::<ActiveGun>();
     }
@@ -18,10 +29,4 @@ pub fn handle_gun_replacement(
     let spawned_gun = on.gun_to_spawn.spawn(&mut commands);
 
     commands.entity(spawned_gun).insert(ActiveGun);
-
-    // gun_bag.add_gun(on.gun_to_spawn.to_string(), spawned_gun);
-
-    return;
-
-    // TODO: Add Gun replace logic
 }
