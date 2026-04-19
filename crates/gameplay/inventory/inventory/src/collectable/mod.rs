a4761329ecfc8fb24ad62d2f817a9ead86ab4fec
dossancto <dossantossilvalucas963@gmail.com>
2026-04-18 23:54:09 -0300

    remove macros

---
+++ crates/gameplay/inventory/inventory/src/collectable/mod.rs
@@ -0,0 +1,65 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct Collectable;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::collect_action::Collect;
+
+    #[derive(Component, Clone)]
+    struct Money {
+        pub amount: f64,
+    }
+
+    #[derive(Resource, Default)]
+    struct PlayerWallet(f64);
+
+    #[derive(Event)]
+    struct TriggerCollect {
+        pub entity: Entity,
+    }
+
+    #[test]
+    fn test_get_event_returns_money_collected() {
+        let mut app = App::new();
+        app.init_resource::<PlayerWallet>();
+
+        app.add_observer(|on: On<Collect<Money>>, mut wallet: ResMut<PlayerWallet>| {
+            wallet.0 = wallet.0 + on.value.amount;
+        });
+
+        app.add_observer(
+            |on: On<TriggerCollect>, mut commands: Commands, query: Query<&Collectable>| {
+                let entity = on.entity;
+
+                let Ok(collectable) = query.get(entity) else {
+                    panic!("Entity does not have a Collectable<Money> component");
+                };
+
+                let value = "123";
+
+                let event_to_trigger = Collect::new(value, entity.clone());
+
+                commands.trigger(event_to_trigger);
+            },
+        );
+
+        let spawned_money = {
+            let money = Money { amount: 10.0 };
+
+            app.world_mut().spawn((money.clone(), Collectable)).id()
+        };
+
+        app.world_mut().trigger(TriggerCollect {
+            entity: spawned_money.clone(),
+        });
+
+        app.update();
+
+        let wallet = app.world().resource::<PlayerWallet>();
+
+        assert_eq!(wallet.0, 10.0);
+    }
+}
--- crates/gameplay/inventory/inventory/src/lib.rs
+++ crates/gameplay/inventory/inventory/src/lib.rs
@@ -1,37 +1,6 @@
-#[cfg(test)]
-mod tests {
-    use bevy::prelude::*;
-    use inventory_core::{Collect, CollectItemAction};
+use bevy::prelude::*;
 
-    #[derive(Component, Clone)]
-    pub struct Money {
-        pub amount: f64,
-    }
+pub mod collect_action;
+pub mod collectable;
 
-    #[derive(Resource, Default)]
-    struct PlayerWallet(f64);
-
-    #[test]
-    fn test_get_event_returns_money_collected() {
-        let mut app = App::new();
-        app.init_resource::<PlayerWallet>();
-
-        app.add_observer(|on: On<Collect<Money>>, mut wallet: ResMut<PlayerWallet>| {
-            wallet.0 = wallet.0 + on.value.amount;
-        });
-
-        let money = Money { amount: 10.0 };
-
-        let spawned_money = app.world_mut().spawn(money.clone()).id();
-
-        let event_to_trigger = Collect::new(money, spawned_money.clone());
-
-        app.world_mut().trigger(event_to_trigger);
-
-        app.update();
-
-        let wallet = app.world().resource::<PlayerWallet>();
-
-        assert_eq!(wallet.0, 10.0);
-    }
-}
+pub fn plugin(_app: &mut App) {}
