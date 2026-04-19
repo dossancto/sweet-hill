0eacbb7d533052f9ed786266a3e9012931a711e2
dossancto <dossantossilvalucas963@gmail.com>
2026-04-19 14:34:42 -0300

    center all tests

--- crates/gameplay/inventory/inventory/src/collect/collect_action.rs
+++ crates/gameplay/inventory/inventory/src/collect/collect_action.rs
@@ -36,39 +36,3 @@ impl<E> Collect<E> {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[derive(Component, Clone)]
-    pub struct Money {
-        pub amount: f64,
-    }
-
-    #[derive(Resource, Default)]
-    struct PlayerWallet(f64);
-
-    #[test]
-    fn test_get_event_returns_money_collected() {
-        let mut app = App::new();
-        app.init_resource::<PlayerWallet>();
-
-        app.add_observer(|on: On<Collect<Money>>, mut wallet: ResMut<PlayerWallet>| {
-            wallet.0 = wallet.0 + on.value.amount;
-        });
-
-        let money = Money { amount: 10.0 };
-
-        let spawned_money = app.world_mut().spawn(money.clone()).id();
-
-        let event_to_trigger = Collect::new(money, spawned_money.clone());
-
-        app.world_mut().trigger(event_to_trigger);
-
-        app.update();
-
-        let wallet = app.world().resource::<PlayerWallet>();
-
-        assert_eq!(wallet.0, 10.0);
-    }
-}
--- crates/gameplay/inventory/inventory/src/collect/mod.rs
+++ crates/gameplay/inventory/inventory/src/collect/mod.rs
@@ -5,4 +5,7 @@ pub mod collect_action;
 pub mod collect_trigger;
 pub mod collectable;
 
+#[cfg(test)]
+mod tests;
+
 pub(super) fn plugin(_app: &mut App) {}
