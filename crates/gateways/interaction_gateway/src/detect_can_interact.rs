36e3dded9871ace2b1fe48bbcdd281c1f7519789
dossancto <dossantossilvalucas963@gmail.com>
2026-04-22 22:43:14 -0300

    split the code

---
+++ crates/gateways/interaction_gateway/src/detect_can_interact.rs
@@ -0,0 +1,60 @@
+use avian3d::prelude::{SpatialQuery, SpatialQueryFilter};
+use bevy::prelude::*;
+use interaction::interaction::components::{CanInteract, Interactable};
+use states::{
+    interaction::{InteractionSourceCamera, InterationConfiguration},
+    player::Player,
+};
+use third_party::avian3d::CollisionLayer;
+
+pub(super) fn plugin(app: &mut App) {
+    // TODO: Add some flag to only run this system if the level is running.
+    // It also should be possible to disable this system.
+    app.add_systems(Update, can_interact);
+}
+
+fn can_interact(
+    interactables_q: Query<&Interactable>,
+    can_interact_q: Query<Entity, With<CanInteract>>,
+    interation_configuration: Single<&InterationConfiguration>,
+    player: Single<(&GlobalTransform, Entity), With<InteractionSourceCamera>>,
+    spatial_query: SpatialQuery,
+    player_collider: Single<Entity, With<Player>>,
+    mut commands: Commands,
+) {
+    let (player, _) = player.into_inner();
+
+    let camera_transform = player.compute_transform();
+
+    let interation_configuration = interation_configuration.into_inner();
+
+    let forward = camera_transform.forward();
+
+    let hit = spatial_query.cast_ray(
+        camera_transform.translation,
+        forward,
+        interation_configuration.interaction_distance,
+        true,
+        &SpatialQueryFilter::from_mask(CollisionLayer::Interactable)
+            .with_excluded_entities([*player_collider]),
+    );
+
+    for entity in can_interact_q.iter() {
+        if let Some(hit) = hit {
+            if hit.entity == entity {
+                continue;
+            }
+        }
+        commands.entity(entity).remove::<CanInteract>();
+    }
+
+    let Some(hit) = hit else {
+        return;
+    };
+
+    let Ok(_is_hitteable) = interactables_q.get(hit.entity) else {
+        return;
+    };
+
+    commands.entity(hit.entity).insert_if_new(CanInteract);
+}
---
+++ crates/gateways/interaction_gateway/src/ui.rs
@@ -0,0 +1,57 @@
+use bevy::prelude::*;
+use interaction::interaction::components::CanInteract;
+use states::interaction::InteractionSourceCamera;
+
+pub(super) fn plugin(app: &mut App) {
+    app.add_observer(setup_can_interact_ui);
+    app.add_observer(clear_can_interact_text);
+
+    app.add_observer(show_can_interact_text);
+    app.add_observer(hide_can_interact_text);
+}
+
+#[derive(Component)]
+struct CanInteractText;
+
+fn setup_can_interact_ui(_on: On<Add, InteractionSourceCamera>, mut commands: Commands) {
+    commands.spawn((
+        Name::new("Can interact text"),
+        Text::new("Press E to interact"),
+        CanInteractText,
+        Visibility::Hidden,
+        Node {
+            position_type: PositionType::Absolute,
+            bottom: px(12),
+            left: px(12),
+            ..default()
+        },
+    ));
+}
+
+fn clear_can_interact_text(
+    _on: On<Remove, InteractionSourceCamera>,
+    can_interact_q: Query<Entity, With<CanInteractText>>,
+    mut commands: Commands,
+) {
+    for visibility in can_interact_q.iter() {
+        commands.entity(visibility).despawn();
+    }
+}
+
+fn show_can_interact_text(
+    _on: On<Add, CanInteract>,
+    mut can_interact_q: Query<&mut Visibility, With<CanInteractText>>,
+) {
+    for mut visibility in can_interact_q.iter_mut() {
+        *visibility = Visibility::Visible;
+    }
+}
+
+fn hide_can_interact_text(
+    _on: On<Remove, CanInteract>,
+    mut can_interact_q: Query<&mut Visibility, With<CanInteractText>>,
+) {
+    for mut visibility in can_interact_q.iter_mut() {
+        *visibility = Visibility::Hidden;
+    }
+}
